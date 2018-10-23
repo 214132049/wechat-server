527a5125688ccf2d04d21b331b402764f829a5ea
DESKTOP-7GDUUR0\liwenli <214132049@qq.com>
2018-10-23 21:53:24 +0800

    更换成163邮箱

--- email/index.js
+++ email/index.js
@@ -13,14 +13,14 @@ var sendmail = function(data) {
   var option = {
     from: config.email.auth.user,
     to: "leasing@l-region.com",
-    subject: "客户预约了，快来查看吧！",
+    subject: `客户[${data.username}]预约了，快来查看吧！`,
     html: `<h1>预约客户信息:</h1><p>姓名：${data.username}</p><p>邮箱：${data.email}</p><p>手机号码：${data.tel}</p><p>预约中心：${data.center}</p>`
   };
   transporter.sendMail(option, function(error, response) {
     if (error) {
-      console.log("fail: " + error);
+      console.log("email-fail: " + error);
     } else {
-      console.log("success: " + response.message);
+      console.log("email-success: " + response.message);
     }
   });
 };
